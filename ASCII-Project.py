a00869cd4796a4f94f53d1747a72b5b415857cf3
BlaiseD <blaise.delforce@outlook.com>
2024-12-17 08:17:53 +0800

    Update ASCII-Project.py

--- ASCII-Project.py
+++ ASCII-Project.py
@@ -1,1 +1,22 @@
 #My names Blaise and here is the first project I have developed on Codecademy
+
+print("""
+BBBB 
+B   B
+B   B
+BBBB 
+B   B
+B   B
+BBBB
+""")
+
+
+print("""
+DDDD 
+D   D
+D   D
+D   D
+D   D
+D   D
+DDDD 
+""")
